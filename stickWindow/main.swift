89d9595b53f8892aa95315b0e35315d61f91fdf2
Nutan Niraula <nutan@Nutans-MacBook-Pro.local>
2018-12-10 15:42:27 +0545

    Initial Commit

---
+++ stickWindow/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  stickWindow
+//
+//  Created by Nutan Niraula on 12/10/18.
+//  Copyright © 2018 Nutan Niraula. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
